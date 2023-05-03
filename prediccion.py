398f224f1ad0a5ec5a7b07cc29ded4df19f3bcc8
valenting13 <76535058+valenting13@users.noreply.github.com>
2023-05-03 17:45:01 -0500

    Update prediccion.py

--- prediccion.py
+++ prediccion.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import openpyxl
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 import joblib
